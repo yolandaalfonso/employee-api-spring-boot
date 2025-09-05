6e535e0303b08d476fe26d533824f3f92d803fa8
Yolanda <yolandaalfonsolopez@gmail.com>
2025-09-05 15:28:06 +0200

    test update

--- src/test/java/SubjectServiceImplTest.java
+++ src/test/java/SubjectServiceImplTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
